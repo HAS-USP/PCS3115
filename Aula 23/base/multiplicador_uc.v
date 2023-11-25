c459a2552b07c2d559e52366252dcd881639f9ca
Solano-Nascimento <132530042+Solano-Nascimento@users.noreply.github.com>
2023-11-24 21:07:19 -0300

    Update multiplicador_uc.v prototype

--- Aula 23/base/multiplicador_uc.v
+++ Aula 23/base/multiplicador_uc.v
@@ -4,27 +4,93 @@ module multiplicador_uc(
             a_ld, b_ld, q_ld, cnt_ld, done
 );
 
-parameter START = 3'b000, EN_LOAD = 3'b001, EVAL = 3'b010, SUM = 3'b011, SHIFT = 3'b100, DONE = 3'b101;
+parameter WAIT = 3'b000, EN_LOAD = 3'b001, EVAL = 3'b010, SUM = 3'b011, SHIFT = 3'b100, DONE = 3'b101;
 
 wire clk, rst, start, qlsb, zero;
 wire a_rst, a_en, b_en, q_en, cnt_en, a_ld, b_ld, q_ld, cnt_ld, done;
 reg [2:0] state;
-assign state = START;
+assign state = WAIT;
 
+always @* begin
+     case(state)
+          WAIT: begin
+               a_en = 0;
+               b_en = 0;
+               q_en = 0;
+               cnt_en = 0;
+               a_ld = 0;
+               b_ld = 0;
+               q_ld = 0;
+               cnt_ld = 0;
+               done = 0;
+          end
+          EN_LOAD: begin
+               a_en=1;
+               b_en=1;
+               q_en=1;
+               cnt_en=1;
+               a_ld=1;
+               b_ld=1;
+               q_ld=1;
+               cnt_ld=1;
+          end
+          EVAL: begin
+               a_en = 0;
+               b_en = 0;
+               q_en = 0;
+               cnt_en = 0;
+               a_ld = 0;
+               b_ld = 0;
+               q_ld = 0;
+               cnt_ld = 0;
+               done = 0; 
+          end
+          SUM: begin
+               a_en = 0;
+               b_en = 0;
+               q_en = 0;
+               cnt_en = 0;
+               a_ld = 0;
+               b_ld = 0;
+               q_ld = 0;
+               cnt_ld = 0;
+               done = 0;    
+          end
+          SHIFT: begin
+               a_en = 1;
+               b_en = 0;
+               q_en = 1;
+               cnt_en = 0;
+               a_ld = 0;
+               b_ld = 0;
+               q_ld = 0;
+               cnt_ld = 0;
+               done = 0;
+          end
+          DONE: begin
+               a_en = 0;
+               b_en = 0;
+               q_en = 0;
+               cnt_en = 0;
+               a_ld = 0;
+               b_ld = 0;
+               q_ld = 0;
+               cnt_ld = 0;
+               done = 1;
+          end
 always @(posedge clk or posedge rst) begin
     if (rst) begin
           a_rst = 1;
         end
     end else begin
         case (state)
-            START: begin
+            WAIT: begin
                     if (start)
                          state = EN_LOAD;
                 end
             EN_LOAD: begin
-                    a_en=1, b_en=1, q_en=1, cnt_en=1;
-                    a_ld=1, b_ld=1, q_ld=1, cnt_ld=1;
-                    state = EVAL;
+                    
+                         state = EVAL;
                 end
             EVAL: begin
                     if (qlsb)
@@ -33,20 +99,25 @@ always @(posedge clk or posedge rst) begin
                          state = SHIFT;   
             end
             SUM: begin
-                state = SHIFT;
+                     state = SHIFT;
             end
             SHIFT: begin
-                 a_en=1, q_en=1;
-                 a_ld=0, q_ld=0;
+                      if(z == 1)
+                           state = DONE;
+                      else
+                           state = EVAL;
             end
             DONE: begin
-                start <= 0;
+                      if(~start)
+                           state = WAIT;
+                      
             end
             default: begin
-                // Do nothing
+                
             end
         endcase
     end
 end
 
+          
 endmodule
